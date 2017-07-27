a395a4672c71e6d262d63194b7faabe6f83ec904
徐乔伦 <xql@feiniubus.com>
2017-07-27 16:45:26 +0800

    添加error wrap包

---
+++ errwrap/errwrap.go
@@ -0,0 +1,34 @@
+package errwrap
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+// ErrorWrap is a http error wrap
+type ErrorWrap struct {
+	StatusCode int
+	What       error
+}
+
+func (w *ErrorWrap) MakeError() error {
+	return fmt.Errorf("%s: %s", http.StatusText(w.StatusCode), w.What.Error())
+}
+
+// Wrap defines a ErrorWrap
+func Wrap(code int, err string) *ErrorWrap {
+	return &ErrorWrap{
+		StatusCode: code,
+		What:       errors.New(err),
+	}
+}
+
+// Wrapf defines a ErrorWrap
+func Wrapf(code int, format string, a ...interface{}) *ErrorWrap {
+	err := fmt.Errorf(format, a...)
+	return &ErrorWrap{
+		StatusCode: code,
+		What:       err,
+	}
+}
